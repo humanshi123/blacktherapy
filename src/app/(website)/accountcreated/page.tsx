8f05ac19dde14b115fbd8b3daaa88f0dde8c0c3e
humanshi123 <humanshi.auspicioussoft@gmail.com.com>
2024-10-11 18:04:15 +0530

    11-10-2024

--- src/app/(website)/accountcreated/page.tsx
+++ src/app/(website)/accountcreated/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react';
 import Image from 'next/image';
-import kerry from "../../assets/images/kerry.png"
+import kerry from "@/assets/images/kerry.png"
 import Link from 'next/link';
 import { ButtonSvg } from '@/utils/svgicons';
 
